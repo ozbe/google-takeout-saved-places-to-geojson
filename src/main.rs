25b4088e267134d3b0bf71d73d5806ec76688b91
Josh Aaseby <1372945+ozbe@users.noreply.github.com>
2021-01-29 17:23:08 +1100

    Nextcloud support (#2)
    * update geojson properties so it can be imported into Nextcloud Maps
    
    * remove extra newline from process output; fix address casing so it will be imported correctly
    
    * update README with latest output and remove debug command
    
    * remove comment from PlaceDetailsResult into Feature

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,13 @@ struct PlaceDetailsLocation {
 impl Into<Feature> for PlaceDetailsResult {
     fn into(self) -> Feature {
         let mut properties = Map::new();
-        properties.insert(
+        let mut location = Map::new();
+        location.insert(
             "address".to_string(),
             JValue::String(self.formatted_address),
         );
-        properties.insert("name".to_string(), JValue::String(self.name));
+        properties.insert("Location".to_string(), JValue::Object(location));
+        properties.insert("Title".to_string(), JValue::String(self.name));
 
         let geometry = Geometry::new(Value::Point(vec![
             self.geometry.location.lng,
@@ -130,7 +132,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let serialized = GeoJson::from(feature_collection).to_string();
-    println!("{:}", serialized);
+    print!("{}", serialized);
 
     Ok(())
 }
@@ -241,11 +243,13 @@ Chubby Noodle North Beach,,https://www.google.com/maps/place/Chubby+Noodle+North
         };
         let expected = {
             let mut properties = Map::new();
-            properties.insert(
-                "address".to_string(),
+            let mut location = Map::new();
+            location.insert(
+                "Address".to_string(),
                 JValue::String("123 Fake St, Springfield".to_string()),
             );
-            properties.insert("name".to_string(), JValue::String("Paper Co".to_string()));
+            properties.insert("Location".to_string(), JValue::Object(location));
+            properties.insert("Title".to_string(), JValue::String("Paper Co".to_string()));
 
             Feature {
                 bbox: None,
